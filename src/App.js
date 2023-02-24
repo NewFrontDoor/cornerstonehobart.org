9842533700bb94df7b46092aa9ae507955699b8c
Bratalix <bratalix@gmail.com>
2023-02-24 13:38:28 +1100

    Add popup

--- src/App.js
+++ src/App.js
@@ -1,81 +1,82 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { ThemeProvider } from 'mineral-ui/themes';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ThemeProvider } from "mineral-ui/themes";
 
-import configuration from './config/config.js';//eslint-disable-line
-import Navigation from './components/Navigation';
-import HomePageWrapper from './components/homepage/HomePageWrapper';
-import OtherPageWrapper from './components/OtherPageWrapper';
-import Footer from './components/Footer';
+import configuration from "./config/config.js"; //eslint-disable-line
+import Navigation from "./components/Navigation";
+import HomePageWrapper from "./components/homepage/HomePageWrapper";
+import OtherPageWrapper from "./components/OtherPageWrapper";
+import Footer from "./components/Footer";
 
-import './assets/css/mediaelement/mediaelementplayer.min.css';
+import "./assets/css/mediaelement/mediaelementplayer.min.css";
 
-import './assets/css/simplenews/simplenews.css';
-import './assets/css/alignmentstyles.css';
+import "./assets/css/simplenews/simplenews.css";
+import "./assets/css/alignmentstyles.css";
 
-import './assets/css/font-awesome/font-awesome.min.css';
+import "./assets/css/font-awesome/font-awesome.min.css";
 
-import './assets/css/tb_megamenu/bootstrap.css';
-import './assets/css/tb_megamenu/base.css';
-import './assets/css/tb_megamenu/default.css';
-import './assets/css/tb_megamenu/compatibility.css';
+import "./assets/css/tb_megamenu/bootstrap.css";
+import "./assets/css/tb_megamenu/base.css";
+import "./assets/css/tb_megamenu/default.css";
+import "./assets/css/tb_megamenu/compatibility.css";
 
-import './assets/nestor/css/bootstrap.min.css';
-import './assets/nestor/css/ionicons.min.css';
-import './assets/nestor/css/flexslider.css';
-import './assets/nestor/css/style.css';
+import "./assets/nestor/css/bootstrap.min.css";
+import "./assets/nestor/css/ionicons.min.css";
+import "./assets/nestor/css/flexslider.css";
+import "./assets/nestor/css/style.css";
 
-import './assets/css/fontyourface/font.css';
+import "./assets/css/fontyourface/font.css";
 
-import './assets/nestor/css/color/blue.css';
+import "./assets/nestor/css/color/blue.css";
 
-import './assets/css/custom.css';
-import './assets/css/events.css';
-import './assets/css/notification.css';
+import "./assets/css/custom.css";
+import "./assets/css/events.css";
+import "./assets/css/notification.css";
 
-//import SiteNotification from './components/SiteNotification';
+import SiteNotification from "./components/SiteNotification";
 
-const notificationID = '2019DecCornerstoneService';
+const notificationID = "2023greekfestival";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { showNotification: localStorage.getItem(notificationID) || true }
-  }
+	constructor(props) {
+		super(props);
+		this.state = { showNotification: localStorage.getItem(notificationID) || true };
+	}
 
-  componentDidMount() {
-    const showNotification = localStorage.getItem(notificationID);
-    this.setState({ showNotification: !showNotification });
-  }
+	componentDidMount() {
+		const showNotification = localStorage.getItem(notificationID);
+		this.setState({ showNotification: !showNotification });
+	}
 
-  onNotificationClose = () => {
-    this.setState({ showNotification: false });
-    localStorage.setItem(notificationID, false)
-  };
+	onNotificationClose = () => {
+		this.setState({ showNotification: false });
+		//set localstorage to only show notification once if desired
+		//localStorage.setItem(notificationID, false);
+	};
 
-  render() {
-    return (
-      <ThemeProvider>
-        <Router>
-          <div className="App">
-            <Route path="*" component={Navigation} />
-            <Route exact path="/" component={HomePageWrapper} />
-            <Route path="/:path" component={OtherPageWrapper} />
+	render() {
+		return (
+			<ThemeProvider>
+				<Router>
+					<div className="App">
+						<Route path="*" component={Navigation} />
+						<Route exact path="/" component={HomePageWrapper} />
+						<Route path="/:path" component={OtherPageWrapper} />
 
-            {/*<Route
-              path={'*'}
-              component={() => <SiteNotification
-                showNotification={this.state.showNotification}
-                onClose={this.onNotificationClose}
-              />}
-              />*/}
+						<Route
+							exact
+							path={"/"}
+							component={() => (
+								<SiteNotification showNotification={this.state.showNotification} onClose={this.onNotificationClose} />
+							)}
+						/>
 
-            <Route path="*" component={Footer} />
-          </div>
-        </Router>
-      </ThemeProvider>
-    );
-  }
+						<Route path="*" component={Footer} />
+					</div>
+				</Router>
+			</ThemeProvider>
+		);
+	}
 }
 
 export default App;
--- src/components/SiteNotification.js
+++ src/components/SiteNotification.js
@@ -1,47 +1,46 @@
 /* eslint-disable */
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Popup from 'reactjs-popup';
-
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Popup from "reactjs-popup";
 
 class SiteNotification extends Component {
-    constructor(props) {
-        super(props);
-    }
+	constructor(props) {
+		super(props);
+	}
 
+	render() {
+		return this.props.showNotification ? (
+			<section className="site-notification">
+				<Popup modal defaultOpen={true} closeOnDocumentClick={false} onClose={() => this.props.onClose()}>
+					{(close) => (
+						<div className="notification-popup">
+							<span>
+								<a className="close" onClick={close}>
+									Close &times;
+								</a>
+								{this.props.content}
 
-    render() {
-        return (
-            this.props.showNotification ? (<section className="site-notification">
-                <Popup modal defaultOpen={true} closeOnDocumentClick={false} onClose={() => this.props.onClose()}>
-                    {close => (
-                        <div className="notification-popup">
-                            <span>
-                                <a className="close" onClick={close}>
-                                    Close &times;
-        </a>
-                                {this.props.content}
+								<div className="header">
+									<h3>Welcome to Cornerstone Hobart</h3>
+								</div>
 
-                                <div className="header"><h3>Welcome to Cornerstone Hobart</h3></div>
-
-                                <h5 className="text-center">
-                                    <p>At Cornerstone, we want to remain socially and spiritually connected, however we do recognise the prudence of physical distancing and the need to flatten the curve of the spread of COVID-19.</p>
-
-                                    <p>We are no longer able to physically gather together, but please tune in to our YouTube channel at 10am each Sunday and let’s continue to worship together.</p>
-
-                                    <p>You will find our YouTube broadcasts <a href="https://www.youtube.com/channel/UCF-sudt_zfl2Ni8eR9ayVHQ">here</a>.</p>
-                                </h5>
-
-
-
-
-                            </span>
-                        </div>
-                    )}
-                </Popup >
-            </section >) : ''
-        );
-    }
+								<h5 className="text-center">
+									<p>
+										Due to the Greek Festival this weekend, Cornerstone Church Service for Sunday 26/2/2023 will be held
+										in the Italian Hall, 77 Federal Street North Hobart.
+									</p>
+									<p>Federal Street will be closed from Argyle Street to Thomas Street.</p>
+									<p>Access to the Italian Hall will be from the Elizabeth Street end of Federal Street.</p>
+								</h5>
+							</span>
+						</div>
+					)}
+				</Popup>
+			</section>
+		) : (
+			""
+		);
+	}
 }
 
 export default SiteNotification;
