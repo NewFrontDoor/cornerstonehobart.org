847a1c2751c64c77852d2e699626ee6641bc80d5
Will Ho <wkayi076@gmail.com>
2025-02-14 10:00:29 +0800

    pop-up

--- src/components/homepage/HomePageContent.js
+++ src/components/homepage/HomePageContent.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import LatestSermon from './LatestSermon';
 import WhereToFindUs from './WhereToFindUs';
 import UpcomingEvents from './UpcomingEventsElvanto';
+import SiteNotification from '../SiteNotification';
 
 class HomePageContent extends Component {
   render() {
@@ -11,6 +12,12 @@ class HomePageContent extends Component {
         <div className="content-2 bg-color-white text-color-default" >
           <div className="container">
             <div className="row">
+
+            <SiteNotification 
+            showNotification={true} 
+            content= "Please note that THIS Sunday, the 16th of February 2025, Cornerstone will be meeting at the Polish Club instead of our usual location.  The address is 20 New Town Rd, New Town 7008."
+            onClose={() => console.log("Notification closed")}
+          />
               <LatestSermon />
               <WhereToFindUs />
               <UpcomingEvents />
