812d959ac068ed2fc55b24f4a6c7f4c2e5b90be1
Will Ho <wkayi076@gmail.com>
2025-02-19 15:51:32 +0800

    removed notification

--- src/components/homepage/HomePageContent.js
+++ src/components/homepage/HomePageContent.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import LatestSermon from './LatestSermon';
 import WhereToFindUs from './WhereToFindUs';
 import UpcomingEvents from './UpcomingEventsElvanto';
-import SiteNotification from '../SiteNotification';
 
 class HomePageContent extends Component {
   render() {
@@ -13,11 +12,7 @@ class HomePageContent extends Component {
           <div className="container">
             <div className="row">
 
-            <SiteNotification 
-            showNotification={true} 
-            content= "Please note that THIS Sunday, the 16th of February 2025, Cornerstone will be meeting at the Polish Club instead of our usual location.  The address is 20 New Town Rd, New Town 7008."
-            onClose={() => console.log("Notification closed")}
-          />
+   
               <LatestSermon />
               <WhereToFindUs />
               <UpcomingEvents />
